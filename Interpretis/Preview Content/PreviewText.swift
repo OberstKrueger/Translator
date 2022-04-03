8d2f7d3795889ee5c4b69da6615b64a8aca6a896
OberstKrueger <ok@oberstkrueger.com>
2022-04-03 06:09:47 -0700

    Added swiftlint to build phase

--- Interpretis/Preview Content/PreviewText.swift
+++ Interpretis/Preview Content/PreviewText.swift
@@ -1,23 +1,47 @@
 #if DEBUG
 var previewText: String = """
-                          # Pickled brunch
+# Pickled brunch
 
-                          Williamsburg sustainable church-key palo santo glossier meditation waistcoat fingerstache viral cardigan raclette. Migas YOLO heirloom, tbh air plant cred typewriter. Single-origin coffee keytar seitan, chambray hella snackwave lumbersexual shoreditch iceland subway tile health goth echo park. Street art sustainable copper mug, gluten-free meggings viral green juice farm-to-table man bun forage. Gluten-free pug pickled sartorial seitan hella, tacos retro sustainable austin shoreditch dreamcatcher.
+Williamsburg sustainable church-key palo santo glossier meditation waistcoat fingerstache viral cardigan raclette. Migas
+YOLO heirloom, tbh air plant cred typewriter. Single-origin coffee keytar seitan, chambray hella snackwave lumbersexual
+shoreditch iceland subway tile health goth echo park. Street art sustainable copper mug, gluten-free meggings viral
+green juice farm-to-table man bun forage. Gluten-free pug pickled sartorial seitan hella, tacos retro sustainable
+austin shoreditch dreamcatcher.
 
-                          ## PBR&B hammock locavore
+## PBR&B hammock locavore
 
-                          XOXO marfa master cleanse, 90's chia ennui skateboard tote bag. Bushwick hella hot chicken ethical kale chips echo park affogato truffaut schlitz art party aesthetic plaid snackwave succulents. Fam slow-carb chia fixie, lo-fi tofu put a bird on it craft beer hoodie tbh tumblr hella snackwave. Bushwick taiyaki selfies heirloom beard authentic ramps coloring book yr cronut twee tofu tumblr. Letterpress offal pop-up, waistcoat salvia umami post-ironic. Bitters venmo forage brooklyn fingerstache ramps.
+XOXO marfa master cleanse, 90's chia ennui skateboard tote bag. Bushwick hella hot chicken ethical kale chips echo park
+affogato truffaut schlitz art party aesthetic plaid snackwave succulents. Fam slow-carb chia fixie, lo-fi tofu put a
+bird on it craft beer hoodie tbh tumblr hella snackwave. Bushwick taiyaki selfies heirloom beard authentic ramps
+coloring book yr cronut twee tofu tumblr. Letterpress offal pop-up, waistcoat salvia umami post-ironic. Bitters venmo
+forage brooklyn fingerstache ramps.
 
-                          Listicle four loko kogi, jianbing scenester viral gluten-free. Mlkshk glossier crucifix, cronut messenger bag four loko enamel pin cliche try-hard hell of tumblr taxidermy. Authentic shaman waistcoat, +1 flannel green juice tote bag actually trust fund master cleanse freegan ennui beard. Cred mustache plaid PBR&B. You probably haven't heard of them woke polaroid freegan franzen la croix tilde mixtape kitsch live-edge. Neutra forage mustache, tumblr bushwick distillery prism locavore. Austin ramps meh meditation pabst craft beer.
+Listicle four loko kogi, jianbing scenester viral gluten-free. Mlkshk glossier crucifix, cronut messenger bag four loko
+enamel pin cliche try-hard hell of tumblr taxidermy. Authentic shaman waistcoat, +1 flannel green juice tote bag
+actually trust fund master cleanse freegan ennui beard. Cred mustache plaid PBR&B. You probably haven't heard of them
+woke polaroid freegan franzen la croix tilde mixtape kitsch live-edge. Neutra forage mustache, tumblr bushwick
+distillery prism locavore. Austin ramps meh meditation pabst craft beer.
 
-                          Heirloom affogato glossier cloud bread. Thundercats kinfolk plaid banh mi beard viral tofu artisan bespoke hot chicken iPhone direct trade vape. Meh humblebrag taxidermy stumptown. Meh whatever iceland tilde, you probably haven't heard of them four dollar toast distillery farm-to-table vegan af keffiyeh PBR&B deep v kickstarter edison bulb.
+Heirloom affogato glossier cloud bread. Thundercats kinfolk plaid banh mi beard viral tofu artisan bespoke hot chicken
+iPhone direct trade vape. Meh humblebrag taxidermy stumptown. Meh whatever iceland tilde, you probably haven't heard of
+them four dollar toast distillery farm-to-table vegan af keffiyeh PBR&B deep v kickstarter edison bulb.
 
-                          Tbh DIY green juice chicharrones, copper mug bitters mlkshk. Umami aesthetic tacos, poke beard bicycle rights offal +1 mumblecore hell of. Air plant meditation PBR&B, pitchfork mumblecore keytar farm-to-table everyday carry irony literally. Quinoa cold-pressed ethical swag, four loko glossier waistcoat brunch vape tacos mlkshk man bun distillery.
+Tbh DIY green juice chicharrones, copper mug bitters mlkshk. Umami aesthetic tacos, poke beard bicycle rights offal +1
+mumblecore hell of. Air plant meditation PBR&B, pitchfork mumblecore keytar farm-to-table everyday carry irony
+literally. Quinoa cold-pressed ethical swag, four loko glossier waistcoat brunch vape tacos mlkshk man bun distillery.
 
-                          Leggings lumbersexual kickstarter subway tile tumeric. Prism tilde ramps occupy chartreuse shaman. Jean shorts cold-pressed kitsch bushwick, typewriter selvage sartorial. Small batch actually twee messenger bag waistcoat tacos organic normcore pitchfork cray unicorn lyft offal palo santo.
+Leggings lumbersexual kickstarter subway tile tumeric. Prism tilde ramps occupy chartreuse shaman. Jean shorts
+cold-pressed kitsch bushwick, typewriter selvage sartorial. Small batch actually twee messenger bag waistcoat tacos
+organic normcore pitchfork cray unicorn lyft offal palo santo.
 
-                          Chillwave crucifix pour-over, blue bottle photo booth echo park pickled everyday carry migas you probably haven't heard of them banjo bespoke bitters seitan poutine. Tilde authentic ethical affogato kogi raclette. Letterpress tumeric etsy hot chicken narwhal, selvage lumbersexual pop-up scenester la croix ramps sartorial. Bicycle rights edison bulb heirloom, pour-over venmo chia twee chicharrones paleo live-edge. Thundercats irony synth tattooed ramps direct trade. Tumblr cliche paleo cardigan XOXO iceland palo santo ugh poke flannel crucifix mixtape seitan.
+Chillwave crucifix pour-over, blue bottle photo booth echo park pickled everyday carry migas you probably haven't heard
+of them banjo bespoke bitters seitan poutine. Tilde authentic ethical affogato kogi raclette. Letterpress tumeric etsy
+hot chicken narwhal, selvage lumbersexual pop-up scenester la croix ramps sartorial. Bicycle rights edison bulb
+heirloom, pour-over venmo chia twee chicharrones paleo live-edge. Thundercats irony synth tattooed ramps direct trade.
+Tumblr cliche paleo cardigan XOXO iceland palo santo ugh poke flannel crucifix mixtape seitan.
 
-                          Locavore messenger bag cronut, synth small batch adaptogen meggings la croix glossier hexagon 3 wolf moon asymmetrical. Butcher man braid tilde cornhole pickled, keytar shaman yuccie echo park XOXO. Vape tilde tumeric vaporware irony typewriter cray, poke jianbing post-ironic locavore. Biodiesel gluten-free slow-carb tbh bushwick sustainable.
-                          """
+Locavore messenger bag cronut, synth small batch adaptogen meggings la croix glossier hexagon 3 wolf moon asymmetrical.
+Butcher man braid tilde cornhole pickled, keytar shaman yuccie echo park XOXO. Vape tilde tumeric vaporware irony
+typewriter cray, poke jianbing post-ironic locavore. Biodiesel gluten-free slow-carb tbh bushwick sustainable.
+"""
 #endif
